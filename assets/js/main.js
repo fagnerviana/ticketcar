13756b701ef009888779da7704ddca9353417f7a
Fagner Viana <fagsantiago@gmail.com>
2024-11-02 00:46:09 -0300

    Realizado atualização da estrutura do site

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// Exemplo de script que exibe uma mensagem de boas-vindas
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Bem-vindo ao Sistema de Estacionamento!");
+});
